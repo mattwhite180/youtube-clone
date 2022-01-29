fa5dade4cbe2c09d78b22f8d69f5d86d6b9762b4
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2022-01-29 12:21:13 -0800

    working on models

--- server/myapp/admin.py
+++ server/myapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Video, Tag, Note
+
+admin.site.site_url = "/myapp"
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "description")
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "video")
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "video",
+        "description",
+    )
+
+
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Note, NoteAdmin)
--- server/myapp/models.py
+++ server/myapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        max_length = 6
+        if len(self.name) > max_length:
+            return self.name[:max_length] + "..."
+        else:
+            return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    def __str__(self):
+        max_length = 6
+        if len(self.name) > max_length:
+            return self.name[:max_length] + "..."
+        else:
+            return self.name
+
+class Note(models.Model):
+    description = models.CharField(max_length=1000)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    def __str__(self):
+        max_length = 6
+        if len(self.description) > max_length:
+            return self.description[:max_length] + "..."
+        else:
+            return self.description
